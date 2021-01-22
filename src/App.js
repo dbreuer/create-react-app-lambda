c4073fb4b01214ac58ac5bbec11f3ef75bc8f26c
David Breuer <david.breuer@bimobject.com>
2021-01-22 14:32:14 +0100

    update

--- src/App.js
+++ src/App.js
@@ -3,35 +3,6 @@ import logo from "./logo.svg"
 import "./App.css"
 import CustomReader from "./CustomReader"
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
-
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.getHTML}>get html</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
-
 class App extends Component {
   render() {
     return (
